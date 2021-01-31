d815c00d288c44fb1ef599471de0bd7b51a462ef
pkk1113_desktop <pkk1113@naver.com>
2021-01-31 10:39:35 +0900

    Add project files.

---
+++ ComputerGraphics/AppBase.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "AppBase.h"
+
+AppBase::AppBase(int _width, int _height) :
+	m_prevX(0.0),
+	m_prevY(0.0),
+	m_height(_height),
+	m_width(_width),
+	m_x(0.0),
+	m_y(0.0),
+	m_deltaX(0.0),
+	m_deltaY(0.0),
+	m_leftBtnPressed(false),
+	m_middleBtnPressed(false),
+	m_rightBtnPressed(false),
+	m_scroll(0.0)
+{
+}
+
+AppBase::~AppBase()
+{
+}
+
+void AppBase::OnCursorPosX(double _x)
+{
+	m_x = _x;
+	m_deltaX = m_x - m_prevX;
+	m_prevX = m_x;
+}
+
+void AppBase::OnCursorPosY(double _y)
+{
+	m_y = _y;
+	m_deltaY = m_y - m_prevY;
+	m_prevY = m_y;
+}
+
+void AppBase::OnMouseWheel(double _scroll)
+{
+	m_scroll = _scroll;
+}
+
+void AppBase::OnMouseLeftButton(bool _pressed)
+{
+	m_leftBtnPressed = _pressed;
+}
+
+void AppBase::OnMouseRightButton(bool _pressed)
+{
+	m_rightBtnPressed = _pressed;
+}
+
+void AppBase::OnMouseMiddleButton(bool _pressed)
+{
+	m_middleBtnPressed = _pressed;
+}
+
+void AppBase::OnFramebufferSize(int _width, int _height)
+{
+	m_width = _width;
+	m_height = _height;
+}
+
+void AppBase::Update(double _deltaTime)
+{
+}                                        
+
+void AppBase::Draw()
+{
+}
---
+++ ComputerGraphics/AppBase.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class AppBase
+{
+private:
+	double m_prevX;
+	double m_prevY;
+
+protected:
+	int m_width;
+	int m_height;
+	double m_x;
+	double m_y;
+	double m_deltaX;
+	double m_deltaY;
+	bool m_leftBtnPressed;
+	bool m_middleBtnPressed;
+	bool m_rightBtnPressed;
+	double m_scroll;
+
+public:
+	AppBase(int _width, int _height);
+	virtual ~AppBase();
+	void OnCursorPosX(double _x);
+	void OnCursorPosY(double _y);
+	void OnMouseWheel(double _scroll);
+	void OnMouseLeftButton(bool pressed);
+	void OnMouseRightButton(bool pressed);
+	void OnMouseMiddleButton(bool pressed);
+	void OnFramebufferSize(int width, int height);
+public:
+	virtual void Update(double _deltaTime);
+	virtual void Draw();
+};
+
