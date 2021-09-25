506e625fc3b02167cb9e9132573dafff03769bbf
pkk1113 <pkk1113@naver.com>
2021-09-25 15:11:04 +0900

    모든 플랫폼에서 같은 문자집합을 쓰도록 수정

--- ComputerGraphics/main.cpp
+++ ComputerGraphics/main.cpp
@@ -1,19 +1,26 @@
+//> ÇÃ·§Æû °áÁ¤(ÇÏ³ª¸¸ È°¼ºÈ­)
+//---------------------------
+#define USE_WINDOWS_BASE 1
+//#define USE_GLFW_BASE 1
+//---------------------------
+#ifdef USE_WINDOWS_BASE
 #include "windows_base.h"
+using namespace windows_base;
+#elif USE_GLFW_BASE
 #include "glfw_base.h"
+using namespace glfw_base;
+#endif
+
+//> Ã¢ ¼³Á¤ °áÁ¤
+#define WINDOW_TITLE "Title"
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 800
+
+//> ¾Û °áÁ¤
 #include "AppBase.h"
 
-
-//////////////////////////////////////////////////////////////////////////
-constexpr int WINDOW_WIDTH = 800;
-constexpr int WINDOW_HEIGHT = 400;
-constexpr char WINDOW_TITLE[] = "Title";
-
-//////////////////////////////////////////////////////////////////////////
 int main()
 {
-	//using namespace glfw_base;
-	using namespace windows_base;
-
 	InitPlatformSettings(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	// TODO:
